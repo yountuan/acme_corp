b1ecc9bf1e970fd000de852c4216c87889cd32e3
yountuan <yountuan@Aidas-MacBook-Air.local>
2024-06-12 18:14:28 +0600

    updated readme with screenshots

--- config/settings.py
+++ config/settings.py
@@ -149,7 +149,6 @@ STORAGES = {
         "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     }}
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
