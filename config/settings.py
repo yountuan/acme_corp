047ba5720f82ed00c867cb1bd906c7a27d8d8d70
yountuan <yountuan@Aidas-MacBook-Air.local>
2024-06-12 18:47:26 +0600

    updated readme with screenshots

--- config/settings.py
+++ config/settings.py
@@ -138,10 +138,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = ()
-STATIC_HOST = os.environ.get("DJANGO_STATIC_HOST", "")
-STATIC_URL = STATIC_HOST + "/static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 STORAGES = {
     "default": {
